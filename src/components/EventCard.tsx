989303f3ec0c204c396d99393edf3819236ef007
Shravan Asati <dev.shravan@protonmail.com>
2024-07-26 21:00:55 +0530

    add favicon + logo and fix events dynamic handler

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -41,7 +41,7 @@ const Icon = ({ navLink }: { navLink: string }) => {
 				xmlns="http://www.w3.org/2000/svg"
 				fill="none"
 				viewBox="0 0 24 24"
-				stroke-width="1.5"
+				strokeWidth="1.5"
 				stroke="currentColor"
 				className="h-4 w-4 text-white stroke-2 hover:stroke-1"
 			>
